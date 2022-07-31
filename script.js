67329324f59a009cc9678e12f98a75d89ad1c7f2
ChiefMasterkim <88433943+ChiefMasterkim@users.noreply.github.com>
2022-07-31 10:54:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".red").click(function(){
+        $("p").hide();
+    });
+    $(document).ready(function(){
+    $("#p1").mouseenter(function(){
+        alert("You entered p1!");
+    });
+});
+});
+$(document).ready(function(){
+    $("#p1").mouseenter(function(){
+        alert("You entered p1!");
+    });
+});
